4c3b84e2280691f6e791c4ec39c5ee2d266c1590
Maximilian Schiller <manimax3@outlook.de>
2023-12-01 22:21:32 +0100

    simple parsing

--- src/main.cpp
+++ src/main.cpp
@@ -137,9 +137,9 @@ std::vector<Token> lex(std::string_view input)
 }
 
 struct Instruction {
-	Token        corresponding_token;
-	std::int64_t forward_jump  = 0;
-	std::int64_t backward_jump = 0;
+	Token       corresponding_token;
+	std::size_t forward_jump  = 0;
+	std::size_t backward_jump = 0;
 };
 
 struct Procedure {
@@ -151,13 +151,13 @@ Token expect(std::span<Token> &input, Token::TokenType type)
 {
 	if (input.empty()) {
 		std::cerr << fmt::format("Expected token of type {}. Got empty sequence\n", static_cast<int>(type));
-		std::exit(1);
+		std::terminate();
 	}
 
 	if (input.front().type != type) {
 		std::cerr << fmt::format("Expected token of type {} got type {} with content {}\n", static_cast<int>(type),
 								 static_cast<int>(input.front().type), input.front().content);
-		std::exit(1);
+		std::terminate();
 	}
 
 	const auto token = input.front();
@@ -167,30 +167,79 @@ Token expect(std::span<Token> &input, Token::TokenType type)
 
 Procedure parse_procedure(std::span<Token> input)
 {
-	Procedure procedure;
+	Procedure proc;
 
 	expect(input, Token::Keyword_Proc);
 	const auto ident = expect(input, Token::Identifier);
-	procedure.name   = ident.content;
+	proc.name        = ident.content;
 	expect(input, Token::Keyword_Do);
 
-	std::stack<std::int64_t> jump_stack;
+	std::stack<std::size_t> jump_stack;
 
 	while (!input.empty()) {
 		const auto &token = input.front();
 		if (token.type == Token::Keyword_End && jump_stack.empty()) {
 			break;
 		}
-
-		auto &inst               = procedure.body.emplace_back();
+		auto &inst               = proc.body.emplace_back();
 		inst.corresponding_token = token;
+		const auto inst_idx      = size(proc.body) - 1;
+
+		switch (token.type) {
+		case Token::Keyword_While: {
+			jump_stack.push(inst_idx);
+			break;
+		}
+		case Token::Keyword_If: {
+			jump_stack.push(inst_idx);
+			break;
+		}
+		case Token::Keyword_Do: {
+			if (jump_stack.empty()) {
+				std::cerr << "Expected branching instruction preceding do instruction\n";
+				std::terminate();
+			}
+
+			auto &branch_inst        = proc.body[jump_stack.top()];
+			branch_inst.forward_jump = inst_idx;
+			inst.backward_jump       = jump_stack.top();
+
+			jump_stack.pop();
+			jump_stack.push(inst_idx);
+			break;
+		}
+		case Token::Keyword_End: {
+			if (jump_stack.empty()) {
+				std::cerr << "Expected do instruction preceding end instruction\n";
+				std::terminate();
+			}
+
+			auto &branch_inst = proc.body[jump_stack.top()];
+			if (branch_inst.corresponding_token.type != Token::Keyword_Do) {
+				std::cerr << "Expected do token before end token\n";
+				std::terminate();
+			}
+			branch_inst.forward_jump = inst_idx;
+			inst.backward_jump       = branch_inst.backward_jump;
+
+			jump_stack.pop();
+			break;
+		}
+		default:
+			break;
+		}
 
 		input = input.subspan(1);
 	}
 
+	if (!jump_stack.empty()) {
+		std::cerr << "Unclosed braching instructions\n";
+		std::terminate();
+	}
+
 	expect(input, Token::Keyword_End);
 
-	return procedure;
+	return proc;
 }
 
 int main()
@@ -198,15 +247,24 @@ int main()
 	constexpr std::string_view input{
 		R"(
 		proc main do
+		while 10 do 10 10 + end
 		10 10 +
+		end
 		)"
 	};
 
-	auto tokens = lex(input);
-	parse_procedure(tokens);
+	auto       tokens = lex(input);
+	const auto proc   = parse_procedure(tokens);
 
 	for (const auto &token : tokens) {
 		std::cout << token.type << " " << token.content << std::endl;
+	}
+
+	std::cout << fmt::format("Found procedure {}\n", proc.name);
+	for (std::size_t i = 0; i < proc.body.size(); ++i) {
+		const auto &inst = proc.body.at(i);
+		std::cout << fmt::format(" I {} Type: {} B: {} F: {}\n", i, inst.corresponding_token.content,
+								 inst.backward_jump, inst.forward_jump);
 	}
 
 	return 0;
